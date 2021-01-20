322151180bb3ac8ad786b4ba65b4352f125fceb8
Maurice Ma <maurice.ma@intel.com>
2021-01-19 19:42:43 -0800

    Add support for Linux payload with InitRd and CmdLine
    A Linux kernel may take InitRamfs and CmdLine. This patch added a
    simple layout to carry all Kernel, InitRamfs and CmdLine file when
    required.
    
    Signed-off-by: Maurice Ma <maurice.ma@intel.com>

--- UniversalPayloadPkg/LinuxLoaderStub/LinuxLoaderStub.c
+++ UniversalPayloadPkg/LinuxLoaderStub/LinuxLoaderStub.c
@@ -7,7 +7,6 @@
 
 #include "LinuxLoaderStub.h"
 
-
 /**
   Linux Loader Stub
 
@@ -24,32 +23,63 @@ SecStartup (
   UINT8                      *End;
   BOOT_PARAMS                *Bp;
   CHAR8                       CmdLineBuf[MAX_CMD_LINE_LEN];
+  LINUX_COMP_HDR             *LnxHdr;
+  UINT32                      InitRdLen;
+  UINT8                      *InitRdBuf;
 
   DEBUG ((DEBUG_INFO, "LinuxLoaderStub ... \n"));
 
-  HobList = Params;
   Ptr = (UINT8 *)((UINTN)SecStartup & ~(SIZE_4KB - 1));
+
+  HobList    = Params;
+  Status     = EFI_NOT_FOUND;
+  InitRdLen  = 0;
+  InitRdBuf  = NULL;
+
   End = Ptr + SIZE_64KB;
   while (Ptr < End) {
     Bp = (BOOT_PARAMS *)Ptr;
     if ((Bp->Hdr.Signature == 0xAA55) && (Bp->Hdr.Header == SETUP_HDR)) {
       break;
     }
+    LnxHdr     = (LINUX_COMP_HDR *)Ptr;
+    if (LnxHdr->Signature == LINUX_HDR_SIGNATURE) {
+      break;
+    }
     Ptr += SIZE_4KB;
   }
+
   if (Ptr == End) {
     DEBUG ((DEBUG_INFO, "Could not locate kernel image !\n"));
   } else {
-    AsciiStrCpyS (CmdLineBuf, MAX_CMD_LINE_LEN, DEF_CMD_LINE);
-    Status = UpldLoadBzImage ((VOID*)Bp, NULL, 0, CmdLineBuf, MAX_CMD_LINE_LEN);
-    if (!EFI_ERROR(Status)) {
-      UpldLinuxBoot (HobList, (VOID *)Bp);
+    if (Bp->Hdr.Signature == 0xAA55) {
+      AsciiStrCpyS (CmdLineBuf, MAX_CMD_LINE_LEN, CmdLineBuf);
+      Status = EFI_SUCCESS;
     } else {
-      DEBUG ((DEBUG_INFO, "Failed to setup kernel image !\n"));
+      if (LnxHdr->InitRd.Len > 0) {
+        InitRdLen = LnxHdr->InitRd.Len;
+        InitRdBuf = Ptr + LnxHdr->InitRd.Off;
+      }
+      if (LnxHdr->CmdLine.Len > 0) {
+        AsciiStrCpyS (CmdLineBuf, MAX_CMD_LINE_LEN, Ptr + LnxHdr->CmdLine.Off);
+      }
+      if (LnxHdr->Kernel.Len > 0) {
+        Bp = (BOOT_PARAMS *)(Ptr + LnxHdr->Kernel.Off);
+        Status = EFI_SUCCESS;
+      }
+    }
+
+    if (!EFI_ERROR(Status)) {
+      Status = UpldLoadBzImage ((VOID*)Bp, InitRdBuf, InitRdLen, CmdLineBuf, MAX_CMD_LINE_LEN);
+      if (!EFI_ERROR(Status)) {
+        UpldLinuxBoot (HobList, (VOID *)Bp);
+      }
+    }
+
+    if (EFI_ERROR(Status)) {
+      DEBUG ((DEBUG_INFO, "Failed to load kernel image !\n"));
     }
   }
-
-  DEBUG ((DEBUG_INFO, "Done\n"));
 
   while (1);
 }
